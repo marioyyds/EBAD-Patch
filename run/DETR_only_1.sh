bb3de7ed9deb8e0c690faa9ce22e334f6ea174f7
mario <marioDu@163.com>
2024-04-08 09:59:47 +0800

    add two shell script

---
+++ run/DETR_only_1.sh
@@ -0,0 +1,3 @@
+/data/hdd3/duhao/miniconda3/envs/EBAD/bin/python ../attack_bb_det1.py --iterw 1\
+--iters 20 \
+--eps 10 \
---
+++ run/DETR_only_10.sh
@@ -0,0 +1,3 @@
+/data/hdd3/duhao/miniconda3/envs/EBAD/bin/python ../attack_bb_det1.py --iterw 10\
+--iters 20 \
+--eps 10 \
